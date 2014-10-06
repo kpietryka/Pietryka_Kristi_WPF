f86fc95458da5235e00b73f24c2cea627cb4cee1
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-05 20:10:19 -0400

    Week 2 Activity 2
    Completed Screencast: Defining Variables

--- AssociatingJS2/js/script.js
+++ AssociatingJS2/js/script.js
@@ -1,3 +1,7 @@
-//JavaScript
+//Defining Variables
 
-var age;
+var age; //declaration
+age = 23; //definition
+//storage container = value
+
+var yearOfBirth = 1991; //declaring and defining on the same line
