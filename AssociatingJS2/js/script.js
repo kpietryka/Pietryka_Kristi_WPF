34f624daf140da5525a0d69aa2136643e5c68dbd
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-07 10:43:50 -0400

    Week 2 Activity 3
    Completed Screencast: Basic Expressions

--- AssociatingJS2/js/script.js
+++ AssociatingJS2/js/script.js
@@ -1,10 +1,6 @@
-//Arrays
+//Expressions
 
-var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
-
-muppetNames[1] = "Rizzo"; //Change index 1 to Rizzo
-muppetNames[2] = "Fozzy"; //Array access notation
-
-var num = 2;
-
-console.log(muppetNames[num]); //What's in index 2?
+var a = 2; //sets up variable and defines it with 2
+var b;
+b = a + 3; //add 3 to a
+console.log(a);
