14303056ec5962187721a25b3e21edb3749360fd
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-07 15:57:52 -0400

    Screencast: Prompt and Alert
    Complete

--- AssociatingJS2/js/script.js
+++ AssociatingJS2/js/script.js
@@ -1,6 +1,8 @@
-//Expressions - Assignment Operators
+//Prompt and Alert
 
-var a = 3;
-//a ++ ; //increment operator.. same as saying a = a + 1
-a -- ; //decrement operator, a = a - 1 or a -= 1
-console.log(a);
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area); //prints to console
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
