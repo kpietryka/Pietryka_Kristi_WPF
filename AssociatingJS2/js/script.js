3aab74770355f75ba7f2ac246359b0e231f855a6
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-07 11:07:04 -0400

    Week 2 Activity 3
    Completed Screencast: Example Expressions

--- AssociatingJS2/js/script.js
+++ AssociatingJS2/js/script.js
@@ -1,6 +1,5 @@
-//Expressions
+//Expressions - Age Example
 
-var a = 2; //sets up variable and defines it with 2
-var b;
-b = a + 3; //add 3 to a
-console.log(a);
+var yearBorn = 1991;
+var age = 2014 - yearBorn;
+console.log(age);
