372cea6d69d73f6f04a0049de0c73660ccd5b18a
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-07 12:45:45 -0400

    Week 2 Activity 3
    Completed Screencast: Using Arrays in Expressions

--- AssociatingJS2/js/script.js
+++ AssociatingJS2/js/script.js
@@ -1,16 +1,6 @@
-//Expressions - PEMDAS - The Order of Operations
+//Expressions - Expressions with Arrays
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+var orangeBins = [234, 567, 883];
 
-//The average is the sum of the items divided by number of items
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4) / 4;
-//console.log(average);
-
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width * 2;
-console.log(perimeter);
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
