06b372b35320d7212fa72d7abda59d09590cd207
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-07 12:38:11 -0400

    Week 2 Activity 3
    Completed Screencast: Order of Operations

--- AssociatingJS2/js/script.js
+++ AssociatingJS2/js/script.js
@@ -1,4 +1,16 @@
-//Expressions - Modulo Operator
+//Expressions - PEMDAS - The Order of Operations
 
-var remainder = 32 % 10; //will return value of 2 because of modulo operator
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//The average is the sum of the items divided by number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4) / 4;
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
