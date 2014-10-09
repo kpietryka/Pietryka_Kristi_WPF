d09415e9129b9ed34ff409b2eaf0a8661e6bb043
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-09 18:36:22 -0400

    Expressions - Personal
    Progress on prompts

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,3 +6,7 @@
 // Define the daily pull-up goals in an array
 var goalPullUps = [2, 4, 6, 8, 10, 12, 14];
 
+// Ask the user how many pull-ups were done on each day
+var day1 = prompt("How many pull-ups did you do on day one?");
+var day2 = prompt("How many pull-ups did you do on day two?");
+var day3 = prompt("How many pull-ups did you do on day three?");
