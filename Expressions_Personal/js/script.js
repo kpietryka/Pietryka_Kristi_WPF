f2991bd095927a2693056222f85b5d8dcab66f95
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-09 18:30:20 -0400

    Expressions - Personal
    Goals defined

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,4 +3,6 @@
 // Expressions Assignment - Personal
 
 // How many pull-ups done in one week, and what is the average number of pull-ups per day compared to the goal?
-var amountPullUps = []
+// Define the daily pull-up goals in an array
+var goalPullUps = [2, 4, 6, 8, 10, 12, 14];
+
