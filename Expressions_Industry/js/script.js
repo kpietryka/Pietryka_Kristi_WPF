64c83330ef0e2ac4553bce53e3e780872cc9299f
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-09 20:07:04 -0400

    Formula added
    in notes

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,7 +2,9 @@
 //October 9, 2014
 //Expressions - Industry (Background Info: I work at a music store)
 
-// Calculate how many square inches of fabric are needed to line the top of a flute case
+// Calculate how many square inches of fabric will need to be ordered to line the top part of a flute case
 
 // Input customer name so that cases do not get mixed up
 var customerName = prompt("Enter the customer's name:");
+
+// Area of a rectangle = length * width
