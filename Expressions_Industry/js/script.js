960713e86d782241f349fb52e1d73e64ab1a80e4
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-09 20:04:50 -0400

    customer name input added

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,6 @@
 //Expressions - Industry (Background Info: I work at a music store)
 
 // Calculate how many square inches of fabric are needed to line the top of a flute case
+
+// Input customer name so that cases do not get mixed up
+var customerName = prompt("Enter the customer's name:");
