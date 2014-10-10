e3321f9660ed597c1baa50c3651b4a1c648b0822
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-09 21:32:51 -0400

    third prompt added

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,4 +10,7 @@ var userName = prompt("What is your name?");
 //Prompt the user for their girlfriend's name
 var girlfriendName = prompt("What is your girlfriend's name?");
 
+//Ask the user how many roses they THINK they should get for their girlfriend
+var userRoses = prompt("How many roses do YOU think you should buy your girlfriend?");
+
 
