df755cb6655363084acd8d52e92759186997f12e
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-09 20:03:40 -0400

    Idea written in notes

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,5 @@
 //Kristi Pietryka
 //October 9, 2014
-//Expressions - Industry
+//Expressions - Industry (Background Info: I work at a music store)
 
+// Calculate how many square inches of fabric are needed to line the top of a flute case
