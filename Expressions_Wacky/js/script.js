b73eef262aebef4b1834b779f69b571d9c976a0e
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-09 21:37:45 -0400

    Fourth prompt added

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,7 +10,10 @@ var userName = prompt("What is your name?");
 //Prompt the user for their girlfriend's name
 var girlfriendName = prompt("What is your girlfriend's name?");
 
-//Ask the user how many roses they THINK they should get for their girlfriend
+//Ask the user how many roses they think they should get for their girlfriend
 var userRoses = prompt("How many roses do YOU think you should buy your girlfriend?");
+
+//Ask the user how many times they have done something to make her mad over the last month
+var multiplier = prompt("How many times have you done something to make her mad this month?");
 
 
