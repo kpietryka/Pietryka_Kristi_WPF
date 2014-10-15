de5b7b87cc12c741d19a03b2428c0d8241801b3b
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-15 15:01:58 -0400

    Last Chance for Gas!
    Variables declared

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,12 @@
+/* Kristi Pietryka
+   October 15, 2014
+   Activity: Conditionals Worksheet */
+
+// Last Chance for Gas!
+// Can the driver make it to the next gas station, or should they stop for gas?
+
+// Write givens as variables
+var efficiency = Number(prompt("Enter the number of miles per gallon of fuel that your car gets: "));
+var gaugeReading = Number(prompt("Enter the percentage of fuel you have left: "));
+var capacity = Number(prompt("Enter your tank's fuel capacity in gallons: "));
+
