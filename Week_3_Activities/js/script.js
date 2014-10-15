c0aee29adfabaabc70f647fe26eb41c767082432
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-15 12:51:46 -0400

    Screencast: else if
    Completed

--- Week_3_Activities/js/script.js
+++ Week_3_Activities/js/script.js
@@ -1,14 +1,18 @@
-//Conditional Logic - Relational Expressions
+//Conditional Logic - else if
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHeight = 45; //height of the kid with the parent
 
 //If the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //you can ride!
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
 }else{
-    //code performed if condition is false
+    //sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
