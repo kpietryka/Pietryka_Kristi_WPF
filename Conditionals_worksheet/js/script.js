adfe3b3c4e4bbaf2a91ffe4f7ed2f0fee250dc93
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-15 20:59:47 -0400

    Assignment Complete - Removed Comments
    Comments removed

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -6,7 +6,7 @@
 // Can the driver make it to the next gas station, or should they stop for gas?
 
 // Write givens as variables
-/*var efficiency = Number(prompt("Enter the number of miles per gallon of fuel that your car gets: "));
+var efficiency = Number(prompt("Enter the number of miles per gallon of fuel that your car gets: "));
 var gaugeReading = Number(prompt("Enter the percentage of fuel you have left: "));
 var capacity = Number(prompt("Enter your tank's fuel capacity in gallons: "));
 var gallonsInTank = (0.01 * gaugeReading) * capacity;
@@ -18,7 +18,7 @@ if(milesInTank <= milesToGo){
     console.log("You only have " + gallonsInTank + " gallons of gas in your tank, better get gas now while you can!");
 }else{
     console.log("Yes, you can make it without stopping for gas!");
-}*/
+}
 
 
 
@@ -26,7 +26,7 @@ if(milesInTank <= milesToGo){
 // Check to ensure that username and password entered by user are correct
 
 // Write givens as variables
-/*var username = prompt("Please enter your username: ");
+var username = prompt("Please enter your username: ");
 var password = prompt("Please enter your password: ");
 var correctUsername = "kpietryka";
 var correctPassword = "FullSail16";
@@ -38,7 +38,7 @@ if(username === correctUsername && password === correctPassword){
     console.log("User not found. Try again.");
 }else if(password != correctPassword){
     console.log("Password does not match our records.");
-}*/
+}
 
 
 
