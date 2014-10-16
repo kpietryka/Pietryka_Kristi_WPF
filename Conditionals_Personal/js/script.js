4a8a6b981de3b4e933cbda41db2938c56b09322b
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-16 19:09:12 -0400

    Added to idea
    in comments

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,4 +3,6 @@
     Conditionals - Personal */
 
 // Decide whether or not a person should work on homework tonight
+// Base the decision on hours of homework to do and number of days until due date
+
 
