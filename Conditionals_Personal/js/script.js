33e74128eb8fe0d9631d234db36c1bf88fe36ebc
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-16 19:10:54 -0400

    Variables declared

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,4 +5,8 @@
 // Decide whether or not a person should work on homework tonight
 // Base the decision on hours of homework to do and number of days until due date
 
+// Declare variables/givens
+var hoursOfWork = Number(prompt("Enter the number of hours of homework you have left: "));
+var daysUntilDue = Number(prompt("Enter the number of days until the homework is due: "));
+
 
