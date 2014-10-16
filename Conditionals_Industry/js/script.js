57a8d3b409125e92a4df9e9c6b0f5dc05d790180
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-16 19:40:11 -0400

    Industry idea written in comments

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,6 @@
+/*  Kristi Pietryka
+    October 16, 2014
+    Conditionals - Industry */
+
+// Should a person be allowed to rent a second instrument?
+// (This is industry-related because I work at a music store.)
