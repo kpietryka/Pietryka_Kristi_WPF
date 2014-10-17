42c274a53f657624721bf7efa1ab538767dfe31d
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-16 20:14:29 -0400

    Variables declared

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,3 +4,8 @@
 
 // Should I read a book today?
 // If the person is bored OR if they have read less than two books this month, they should read a book.
+
+// Declare variables and ask for input
+var bored = prompt("Are you bored?");
+var numberBooks = Number(prompt("How many books have you read this month?"));
+
