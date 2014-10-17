77bb939cbf62202f59979898903b3d67b37f63e2
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-16 20:02:48 -0400

    Idea changed, variables declared

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,5 +2,10 @@
     October 16, 2014
     Conditionals - Industry */
 
-// Should a person be given a used or new instrument for rental?
+// Should a person be allowed to rent an instrument?
 // (This is industry-related because I work at a music store.)
+
+// Declare variables and ask for user input
+var creditScore = Number(prompt("Enter your credit score: "));
+var age = Number(prompt("Enter your age: "));
+
