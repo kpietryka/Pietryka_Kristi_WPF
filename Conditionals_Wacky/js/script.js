3b924a4687f0024ca862c4f9736f5c15bd1b9597
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-16 20:18:08 -0400

    conditional written

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,3 +9,11 @@
 var bored = prompt("Are you bored?");
 var numberBooks = Number(prompt("How many books have you read this month?"));
 
+// Write conditional
+if(bored === "yes" || bored === "Yes"){
+    console.log("You are bored, so you should read a book!");
+}else if(numberBooks < 2){
+    console.log("You haven't read very much this month. Read a book!");
+}else{
+    console.log("You seem too busy to read, but you should try to read a book anyway.");
+}
