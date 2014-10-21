748ec949ce2a878a442a4a0a35b0b88f356c1f98
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-21 16:38:39 -0400

    Screencast: Arguments and Parameters
    Completed

--- Week 4 Screencasts/js/script.js
+++ Week 4 Screencasts/js/script.js
@@ -1,14 +1,9 @@
-// Functions - Variable Scope
+// Functions - Parameters and Arguments
 
-var width = 5;
-
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
-    console.log(area);
+function dogYears(age){ //parameters
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.");
 }
-
-console.log(width);
-
-calcArea();
+var age1 = 4;
+dogYears(age1); //arguments
+dogYears(5);
