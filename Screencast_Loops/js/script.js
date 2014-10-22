ac21f35d9fbce76e66cef43dc2fc82d52adcb02e
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-22 13:24:36 -0400

    Screencast: Do While Loops
    Complete

--- Screencast_Loops/js/script.js
+++ Screencast_Loops/js/script.js
@@ -6,3 +6,12 @@ while (b > 0){  //checks the condition
     console.log(b + " kegs on the wall");
     b--; //decrements or increments the index
 }
+
+
+var c = 10;
+
+do{
+    console.log(c + " kegs on the wall");
+    c--;
+}
+while(c > 0);
