074d30fdbc59d9528958e5a8cf3156adf4ce44c4
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-23 19:46:41 -0400

    Instructions added
    in comments

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,7 @@
+/*  Kristi Pietryka
+    October 23, 2014
+    Functions - Industry  */
+
+// Calculate the amount of money a person will be reimbursed for driving a number of miles
+// Add an hourly bonus
+
