e16d7a22fa91d39a3660d4e387279f2ab9217c1e
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-23 19:06:26 -0400

    function written

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,3 +5,8 @@
 // Calculate the number of steps I've taken while running, based on distance
 // There are an average of 2,000 steps per mile
 
+// Write anonymous function to calculate number of steps taken
+var calcSteps = function(numberMiles){
+    var numberSteps = 2000 * numberMiles;
+    return numberSteps;
+}
