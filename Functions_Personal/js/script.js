8cfca05c5d3730df01b778abd2165aba10a7999b
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-23 17:49:41 -0400

    Idea changed
    Written in comments

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,6 +2,6 @@
     October 22, 2014
     Functions - Personal  */
 
-// Calculate the number of pages read vs my goal for number of pages read
-// Output "read more" or "you've met or exceeded your reading goal"
+// Calculate the number of steps I've taken while running, based on distance
+// There are an average of 2,000 steps per mile
 
