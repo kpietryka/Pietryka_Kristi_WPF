2ed2af25b9c0aed637fc1b43ed5e43e8f5441de7
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-23 17:39:21 -0400

    Instructions written
    in comments

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,3 +2,6 @@
     October 22, 2014
     Functions - Personal  */
 
+// Calculate the number of pages read vs my goal for number of pages read
+// Output "read more" or "you've met or exceeded your reading goal"
+
