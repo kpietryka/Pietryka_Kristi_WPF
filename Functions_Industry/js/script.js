0e663a6873de02ea413b3e8211ddd1f630d4bd2c
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-23 20:01:30 -0400

    Else if conditional added

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -12,5 +12,13 @@ var calcAmountMoney = function(milesDriven, ratePerMile, hoursDriven, hourlyBonu
 }
 
 // Write result as variable
-var paycheck = calcAmountMoney(12, 0.54,0.5, 2);
+var paycheck = calcAmountMoney(83, 0.54,2.5, 2);
 
+// Conditional (else if)
+if (paycheck > 50){
+    console.log("You made $" + paycheck + ". Congratulations!");
+}else if(paycheck === 0){
+    console.log("You made $" + paycheck + ". You did not drive at all this week!");
+}else{
+    console.log("You made $" + paycheck + ". Try to drive more next time!");
+}
