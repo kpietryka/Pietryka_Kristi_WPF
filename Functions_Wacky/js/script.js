350275a4565d30dd6b0c6d140bc4edfe4fc2402b
kpietryka <kristi.pietryka@fullsail.edu>
2014-10-23 20:25:12 -0400

    Notes revised

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,5 +3,6 @@
    Functions - Wacky */
 
 // Calculate the average number of pieces of pizza a person can eat per hour
-// Number of slices * one hour
+// Number of hours divided by number of slices
 
+// Create function for calculating number of slices per hour
